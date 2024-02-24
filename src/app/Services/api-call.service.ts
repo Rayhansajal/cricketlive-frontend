881df9915e78a8e617f58bcc3c3a4db7f63ce1fa
Sajal <rayhansajal1996@gmail.com>
2024-02-24 13:13:16 +0600

    Initial Commit

---
+++ src/app/Services/api-call.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { match } from 'assert';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiCallService {
+
+  constructor(private _httpclient:HttpClient) { }
+  getAllMatches(){
+    return this._httpclient.get(`${environment.apiUrl}/match`)
+  }
+  getLiveMatches(){
+    return this._httpclient.get(`${environment.apiUrl}/match/live`)
+
+  }
+  getPointTable(){
+    return this._httpclient.get(`${environment.apiUrl}/match/point-table`)
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { LiveComponent } from './pages/live/live.component';
+import { PointTableComponent } from './pages/point-table/point-table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"home",
+        component:HomeComponent,
+        title: "Home | CricketLive"
+    },
+    {
+        path:"history",
+        component:HistoryComponent,
+        title: "MatchHistory | CricketLive"
+    },
+    {
+        path:"live",
+        component:LiveComponent,
+        title: "Live Matches | CricketLive"
+    },
+    {
+        path:"point-table",
+        component:PointTableComponent,
+        title: "Point Table | CricketLive"
+    }
+];
